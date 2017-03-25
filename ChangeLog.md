805954ce74e75bfe441eac723cf29ad679951170
Tom Panzarella <tpanzarella@loveparkrobotics.com>
2017-03-25 14:14:26 -0400

    Project now produces debian files for easy installation

---
+++ ChangeLog.md
@@ -0,0 +1,3 @@
+## This file has started tracking ifm3d at 0.1.0
+
+* Initial (alpha) release
--- cmake/modules/ifm3d_version.cmake
+++ cmake/modules/ifm3d_version.cmake
@@ -3,7 +3,7 @@
 ## version numbers
 ################################################
 set(IFM3D_VERSION_MAJOR 0)
-set(IFM3D_VERSION_MINOR 0)
+set(IFM3D_VERSION_MINOR 1)
 set(IFM3D_VERSION_PATCH 0)
 set(IFM3D_VERSION_STRING
     "${IFM3D_VERSION_MAJOR}.${IFM3D_VERSION_MINOR}.${IFM3D_VERSION_PATCH}")
