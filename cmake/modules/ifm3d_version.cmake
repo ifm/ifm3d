ba8261a4449054dd924b79885397395182367d4d
Tom Panzarella <tpanzarella@loveparkrobotics.com>
2018-02-08 11:01:15 -0500

    Updated meta-info for release

--- cmake/modules/ifm3d_version.cmake
+++ cmake/modules/ifm3d_version.cmake
@@ -3,7 +3,7 @@
 ## version numbers
 ################################################
 set(IFM3D_VERSION_MAJOR 0)
-set(IFM3D_VERSION_MINOR 7)
+set(IFM3D_VERSION_MINOR 8)
 set(IFM3D_VERSION_PATCH 0)
 set(IFM3D_VERSION_STRING
     "${IFM3D_VERSION_MAJOR}.${IFM3D_VERSION_MINOR}.${IFM3D_VERSION_PATCH}")
