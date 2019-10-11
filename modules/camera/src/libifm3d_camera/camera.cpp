2c681c2e1cd57cd2b99b73bd7286cf10a76d6c51
Sean Kelly <sean.kelly@ifm.com>
2019-10-11 09:06:09 -0400

    Correct supported version for intrinsics
    - Fix some formatting issues
    - Re-enable the intrinsics unit test
    
    closes #170

--- modules/camera/src/libifm3d_camera/camera.cpp
+++ modules/camera/src/libifm3d_camera/camera.cpp
@@ -105,8 +105,8 @@ const unsigned int ifm3d::O3D_INTRINSIC_PARAM_SUPPORT_MINOR = 23;
 const unsigned int ifm3d::O3D_INTRINSIC_PARAM_SUPPORT_PATCH = 0;
 
 const unsigned int ifm3d::O3D_INVERSE_INTRINSIC_PARAM_SUPPORT_MAJOR = 1;
-const unsigned int ifm3d::O3D_INVERSE_INTRINSIC_PARAM_SUPPORT_MINOR = 25;
-const unsigned int ifm3d::O3D_INVERSE_INTRINSIC_PARAM_SUPPORT_PATCH = 0;
+const unsigned int ifm3d::O3D_INVERSE_INTRINSIC_PARAM_SUPPORT_MINOR = 30;
+const unsigned int ifm3d::O3D_INVERSE_INTRINSIC_PARAM_SUPPORT_PATCH = 4123;
 
 //================================================
 // A lookup table listing the read-only camera
