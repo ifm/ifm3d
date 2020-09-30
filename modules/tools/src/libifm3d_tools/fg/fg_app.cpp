6ea67ccd1424925f12efe2cf29241d1fe2c6a99b
Sachin Bangadkar <Sachin.Bangadkar@ifm.com>
2020-09-30 13:29:12 +0530

    formatting changes

--- modules/tools/src/libifm3d_tools/fg/fg_app.cpp
+++ modules/tools/src/libifm3d_tools/fg/fg_app.cpp
@@ -10,16 +10,16 @@
 ifm3d::FgApp::FgApp(int argc, const char** argv, const std::string& name)
   : ifm3d::CmdLineApp(argc, argv, name)
 {
-	// clang-format off
+  // clang-format off
   this->all_opts_.add_options("global")(
     "nat-port",
     "port number for pcic communication on NAT router",
     cxxopts::value<unsigned short>()->default_value(std::to_string(ifm3d::DEFAULT_NAT_PCIC_PORT)));
   // clang-format on
   this->_Parse(argc, argv);
- 
+
   this->nat_port_ = (*this->vm_)["nat-port"].as<unsigned short>();
-  this->fg_ = std::make_shared<ifm3d::FrameGrabber>(
-    this->cam_,
-    ifm3d::DEFAULT_SCHEMA_MASK,this->nat_port_);
+  this->fg_ = std::make_shared<ifm3d::FrameGrabber>(this->cam_,
+                                                    ifm3d::DEFAULT_SCHEMA_MASK,
+                                                    this->nat_port_);
 }
