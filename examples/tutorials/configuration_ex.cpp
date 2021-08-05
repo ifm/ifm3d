13b1060bbbdec306dea1a05c5f7de252d3ae8bae
Masson, Lola <lola.masson@ifm.com>
2021-08-05 10:29:06 -0400

    set and get complete json conf - cpp example

--- examples/tutorials/configuration_ex.cpp
+++ examples/tutorials/configuration_ex.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <iomanip>
 #include <memory>
 #include <fstream>
 #include <ifm3d/camera.h>
@@ -11,9 +12,19 @@ int main(){
     // Get the current configuration of the camera in JSON format
     json conf = cam->ToJSON();
     // Display and then write to file
-    std::cout << conf << std::endl;
-    std::ofstream file("conf.json");
-    file << conf;
+    std::cout << std::setw(4) << conf << std::endl;
+    std::ofstream file_get("/home/usmasslo/O3R/ifm3d/examples/tutorials/conf_get.json");
+    file_get << std::setw(4) << conf;
+
+    // Configure the device from a configuration file
+    std::ifstream file_set("/home/usmasslo/O3R/ifm3d/examples/tutorials/conf_set.json");
+    file_set >> conf;
+    cam->FromJSON(conf);
+
+    // Check that the configuration worked
+    conf = cam->ToJSON();
+    std::cout << std::setw(4) << conf << std::endl;
+
 
     
 
