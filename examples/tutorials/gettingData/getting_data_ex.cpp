63d76c968e2ac3d99556bc7ca6f24fbdeafe5026
Masson, Lola <lola.masson@ifm.com>
2021-08-09 16:15:00 -0400

    Add timeout check

--- examples/tutorials/gettingData/getting_data_ex.cpp
+++ examples/tutorials/gettingData/getting_data_ex.cpp
@@ -15,8 +15,11 @@ int main(){
     // TODO create multiple grabbers and buffers (one per head). Use vectors?
 
     // Get a frame
-    fg_0->WaitForFrame(im_0.get(), 1000);
-
+    if (! fg_0->WaitForFrame(im_0.get(), 3000))
+    {
+      std::cerr << "Timeout waiting for camera!" << std::endl;
+      return -1;
+    }
     // Do something with the data
     cv::Mat amp;
     cv::Mat dist;  
@@ -24,8 +27,9 @@ int main(){
     amp = im_0->AmplitudeImage();
     dist = im_0->DistanceImage();
 
-    std::cout << amp.at<int>(100, 100) << std::endl;
-    std::cout << dist.at<int>(100, 100) << std::endl;
+    std::cout << dist << std::endl;
+    // std::cout << amp.at<int>(100, 100) << std::endl;
+    // std::cout << dist.at<int>(100, 100) << std::endl;
 
 
     return 0;
