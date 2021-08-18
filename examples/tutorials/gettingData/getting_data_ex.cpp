e338641de21e3f0ac7c3157dff375d87927b6242
Masson, Lola <lola.masson@ifm.com>
2021-08-18 14:53:22 -0400

    Add 2D to getting data tutorial

--- examples/tutorials/gettingData/getting_data_ex.cpp
+++ examples/tutorials/gettingData/getting_data_ex.cpp
@@ -10,27 +10,30 @@ int main(){
     auto cam = ifm3d::O3RCamera::MakeShared();
     // Declare the FrameGrabber and ImageBuffer objects. 
     // One FrameGrabber per camera head (define the port number).
-    auto fg_0 = std::make_shared<ifm3d::FrameGrabber>(cam, 10, 50011);
-    auto im_0 =  std::make_shared<ifm3d::ImageBuffer>();
+    auto fg = std::make_shared<ifm3d::FrameGrabber>(cam, 10, 50012);
+    auto im =  std::make_shared<ifm3d::ImageBuffer>();
     // TODO create multiple grabbers and buffers (one per head). Use vectors?
 
     // Get a frame
-    if (! fg_0->WaitForFrame(im_0.get(), 3000))
+    if (! fg->WaitForFrame(im.get(), 3000))
     {
       std::cerr << "Timeout waiting for camera!" << std::endl;
       return -1;
     }
     // Do something with the data
-    cv::Mat amp;
-    cv::Mat dist;  
+    // For 3D data:
+    // cv::Mat amp;
+    // cv::Mat dist;  
 
-    amp = im_0->AmplitudeImage();
-    dist = im_0->DistanceImage();
+    // amp = im->AmplitudeImage();
+    // dist = im->DistanceImage();
 
-    std::cout << dist << std::endl;
-    // std::cout << amp.at<int>(100, 100) << std::endl;
-    // std::cout << dist.at<int>(100, 100) << std::endl;
+    // std::cout << dist << std::endl; 
 
+    // For 2D data:
+    cv::Mat rgb;
+    rgb = im->JPEGImage();
+    std::cout << rgb << std::endl;
 
     return 0;
 }
