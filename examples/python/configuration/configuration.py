f03dd3bf84d65a1510b470be002ad9e870f66a5e
Masson, Lola <lola.masson@ifm.com>
2021-10-01 17:03:27 -0400

    quick python example for configuration

---
+++ examples/python/configuration/configuration.py
@@ -0,0 +1,18 @@
+# Define the ifm3d objects for the communication
+from ifm3dpy import O3RCamera
+o3r = O3RCamera()
+
+# Get the current configuration
+config = o3r.get()
+
+# Print a little part from the config to verify the configuration
+print(json.dumps(config['device']['swVersion'], indent=4))
+print(config['ports']['port1']['state'])
+
+# Let's change the name of the device
+o3r.set({'device':{'info':{'name':'great_o3r'}}})
+o3r.saveInit()
+
+# Double check the configuration
+config = o3r.get()
+print(config['device']['info']['name'])
