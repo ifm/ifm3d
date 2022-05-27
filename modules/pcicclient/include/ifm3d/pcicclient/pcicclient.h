715c23df8e4757a63c29afc579062a4975061c07
Sachin Bangadkar <Sachin.Bangadkar@ifm.com>
2022-05-27 13:41:54 +0530

    build pcicclient

--- modules/pcicclient/include/ifm3d/pcicclient/pcicclient.h
+++ modules/pcicclient/include/ifm3d/pcicclient/pcicclient.h
@@ -12,7 +12,7 @@
 #include <mutex>
 #include <string>
 #include <thread>
-#include <ifm3d/camera.h>
+#include <ifm3d/device.h>
 
 namespace ifm3d
 {
@@ -34,7 +34,7 @@ namespace ifm3d
      * @param[in] cam The camera instance to grab frames from
      * @param[in] nat_pcic_port pcic port for NAT configuartion
      */
-    PCICClient(ifm3d::Camera::Ptr cam,
+    PCICClient(ifm3d::LegacyDevice::Ptr cam,
                const std::uint16_t pcic_port = ifm3d::PCIC_PORT);
 
     /**
