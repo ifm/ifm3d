d3a599e4056cb8499b269ce447bfe2b975569fda
Sachin Bangadkar <Sachin.Bangadkar@ifm.com>
2022-08-16 14:24:01 +0530

    update python getting_data example

--- examples/o3r/getting_data/getting_data.py
+++ examples/o3r/getting_data/getting_data.py
@@ -1,18 +1,14 @@
 from ifm3dpy import O3R, FrameGrabber, buffer_id
-import asyncio
 
-async def getter(fg):
-      return await fg.wait_for_frame()
 # Initialize the objects
-o3r = O3R('192.168.0.69')
-port='port2'
+o3r = O3R()
 fg = FrameGrabber(o3r, pcic_port=50012)
 
 #set schema and start Grabber
 fg.start([buffer_id.NORM_AMPLITUDE_IMAGE,buffer_id.RADIAL_DISTANCE_IMAGE,buffer_id.XYZ])
 
 # Get a frame
-frame =  getter(fg)
+frame =  fg.wait_for_frame()
 
 # Read the distance image and display a pixel in the center
 dist = frame.get_buffer(buffer_id.RADIAL_DISTANCE_IMAGE)
