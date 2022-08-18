f62ea8b72074735cac05200e37f411567e26aef5
Masson, Lola <lola.masson@ifm.com>
2022-08-18 16:29:40 -0400

    Add callback example to getting data

--- examples/o3r/getting_data/getting_data.py
+++ examples/o3r/getting_data/getting_data.py
@@ -1,16 +1,21 @@
 from ifm3dpy import O3R, FrameGrabber, buffer_id
+import asyncio
 
-# Initialize the objects
-o3r = O3R()
-fg = FrameGrabber(o3r, pcic_port=50012)
+async def main():
+  # Initialize the objects
+  o3r = O3R()
+  fg = FrameGrabber(o3r, pcic_port=50012)
 
-#set schema and start Grabber
-fg.start([buffer_id.NORM_AMPLITUDE_IMAGE,buffer_id.RADIAL_DISTANCE_IMAGE,buffer_id.XYZ])
+  # Set schema and start Grabber
+  fg.start([buffer_id.NORM_AMPLITUDE_IMAGE,buffer_id.RADIAL_DISTANCE_IMAGE,buffer_id.XYZ])
 
-# Get a frame
-frame =  fg.wait_for_frame()
+  # Get a frame
+  frame = await fg.wait_for_frame()
 
-# Read the distance image and display a pixel in the center
-dist = frame.get_buffer(buffer_id.RADIAL_DISTANCE_IMAGE)
-(width, height) = dist.shape
-print(dist[width//2,height//2])
+  # Read the distance image and display a pixel in the center
+  dist = frame.get_buffer(buffer_id.RADIAL_DISTANCE_IMAGE)
+  (width, height) = dist.shape
+  print(dist[width//2,height//2])
+
+if __name__ == "__main__":
+    asyncio.run(main())
