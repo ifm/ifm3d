4019245b767789d1c35ce60477412bf62006f752
Sachin Bangadkar <Sachin.Bangadkar@ifm.com>
2022-12-08 01:05:05 +0530

    add test for testing data grabbing

---
+++ modules/pybind11/test/test_ifm3d_fg2.py
@@ -0,0 +1,34 @@
+#
+# Copyright (C) 2019 ifm electronic, gmbh
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distribtued on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+import pytest
+import time
+
+import ifm3dpy
+
+def test_waitforframe():
+    cam = ifm3dpy.O3R()
+    fg = ifm3dpy.FrameGrabber(cam, 50012)
+    fg.start()
+    
+    count = 0
+    for i in range(10):
+        res,frame = fg.wait_for_frame().wait_for(10000)
+        assert res == True
+        count = count + 1
+    assert count == 10
+
+
+
