cddb0831e3eac9372e4b9dc5f513d6a5a486d8ce
Sachin Bangadkar <Sachin.Bangadkar@ifm.com>
2023-01-12 11:33:39 +0530

    add struct tof_info_v4 and tests

--- modules/deserialize/include/ifm3d/deserialize/struct_tof_info_v4.hpp
+++ modules/deserialize/include/ifm3d/deserialize/struct_tof_info_v4.hpp
@@ -4,128 +4,64 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-#ifndef IFM3D_DESERIALIZE_STRUCT_TOF_INFO_V3_HPP
-#define IFM3D_DESERIALIZE_STRUCT_TOF_INFO_V3_HPP
+#ifndef IFM3D_DESERIALIZE_STRUCT_TOF_INFO_V4_HPP
+#define IFM3D_DESERIALIZE_STRUCT_TOF_INFO_V4_HPP
 
-#include <ifm3d/deserialize/deserialize.h>
+
 #include <array>
 #include <chrono>
 #include <ifm3d/device/device.h>
 #include <ifm3d/device/err.h>
 #include <ifm3d/fg/organizer_utils.h>
 #include <ifm3d/fg/buffer.h>
+#include <ifm3d/deserialize/deserialize_utils.hpp>
+#include <ifm3d/deserialize/struct_tof_info_v3.hpp>
 
 namespace ifm3d
 {
   namespace
   {
-    constexpr auto VERSION_INDEX = 0x0000;
-    constexpr auto DISTANCE_RESOLUTION_INDEX = 0x0004;
-    constexpr auto AMPLITUDE_RESOLUTION_INDEX = 0x0008;
-    constexpr auto AMPLITUDE_NORM_FACTOR_INDEX = 0x000C;
-    constexpr auto EXTRINSIC_OPTICAL_TO_USER_INDEX = 0x0018;
-    constexpr auto INTRINSIC_CALIBRATION_INDEX = 0x0030;
-    constexpr auto INVERSE_INTRINSIC_CALIBRATION_INDEX = 0x00B4;
-    constexpr auto EXPOSURE_TIMESTAMPS_INDEX = 0x0138;
-    constexpr auto EXPOSURE_TIMES_INDEX = 0x0150;
-    constexpr auto ILLUTEMPERATURE_INDEX = 0x015C;
-    constexpr auto MODE_INDEX = 0x0160;
-    constexpr auto IMAGER_INDEX = 0x0180;
+    constexpr auto MEASUREMENT_BLOCK_INDEX = 0x01A0;
+    constexpr auto MEASUREMENT_RANGE_MIN_INDEX = 0x01A4;
+    constexpr auto MEASUREMENT_RANGE_MAX_INDEX = 0x01A8;
   };
 
-  class TofInfoV3
+  class TofInfoV4 : public TofInfoV3
   {
-
   public:
-    using Ptr = std::shared_ptr<TofInfoV3>;
-    struct ExtrinsicOpticToUser
-    {
-      using Ptr = std::shared_ptr<struct ExtrinsicOpticToUser>;
-      float transX; // value in meter
-      float transY; // value in meter
-      float transZ; // value in meter
-      float rotX;   // value in radians
-      float rotY;   // value in radians
-      float rotZ;   // value in radians
-
-      void
-      Read(const uint8_t* data)
-      {
-        transX = mkval<float>(data + sizeof(float) * 0);
-        transY = mkval<float>(data + sizeof(float) * 1);
-        transZ = mkval<float>(data + sizeof(float) * 2);
-        rotX = mkval<float>(data + sizeof(float) * 3);
-        rotY = mkval<float>(data + sizeof(float) * 4);
-        rotZ = mkval<float>(data + sizeof(float) * 5);
-      }
-    };
-    using ExtrinsicOpticToUser = struct ExtrinsicOpticToUser;
-
-    struct Calibration
-    {
-      using Ptr = std::shared_ptr<struct Calibration>;
-      uint32_t modelID;
-      std::array<float, 32> modelParameters;
-      void
-      Read(const uint8_t* data)
-      {
-        modelID = mkval<uint32_t>(data);
-        mkarray<float, 32>(data + sizeof(uint32_t), modelParameters);
-      }
-    };
-
-    using IntrinsicCalibration = struct Calibration;
-    using InverseIntrinsicCalibration = struct Calibration;
+    using Ptr = std::shared_ptr<TofInfoV4>;
 
     void
     Read(const uint8_t* data, size_t size)
     {
-      if (size < tof_info_v3_size)
+      if (size < tof_info_v4_size)
         {
           throw ifm3d::Error(IFM3D_BUFFER_NOT_COMPATIABLE);
         }
+
+     TofInfoV3::Read(data, size);
       const uint8_t* start_ptr = data;
-      version = mkval<std::uint32_t>(start_ptr + VERSION_INDEX);
-      distance_resolution =
-        mkval<float>(start_ptr + DISTANCE_RESOLUTION_INDEX);
-      amplitude_resolution =
-        mkval<float>(start_ptr + AMPLITUDE_RESOLUTION_INDEX);
-      mkarray<float, 3>(start_ptr + AMPLITUDE_NORM_FACTOR_INDEX,
-                        amp_normalization_factors);
-      extrisic_optic_to_user.Read(start_ptr + EXTRINSIC_OPTICAL_TO_USER_INDEX);
-      intrinsic_calibration.Read(start_ptr + INTRINSIC_CALIBRATION_INDEX);
-      inverse_intrinsic_calibration.Read(start_ptr +
-                                         INVERSE_INTRINSIC_CALIBRATION_INDEX);
-      mkarray<uint64_t, 3>(start_ptr + EXPOSURE_TIMESTAMPS_INDEX,
-                           exposure_timestamps_ns);
-      mkarray<uint32_t, 3>(start_ptr + EXPOSURE_TIMES_INDEX, exposure_times_s);
-      illu_temperature = mkval<float>(start_ptr + ILLUTEMPERATURE_INDEX);
-      mkarray<char, 32>(start_ptr + MODE_INDEX, mode);
-      mkarray<char, 32>(start_ptr + IMAGER_INDEX, imager);
+      measurement_block_index =
+        mkval<std::uint32_t>(start_ptr + MEASUREMENT_BLOCK_INDEX);
+      measurement_range_min =
+        mkval<float>(start_ptr + MEASUREMENT_RANGE_MIN_INDEX);
+      measurement_range_max =
+        mkval<float>(start_ptr + MEASUREMENT_RANGE_MAX_INDEX);
     };
 
-    uint32_t version;
-    float distance_resolution;
-    float amplitude_resolution;
-    std::array<float, 3> amp_normalization_factors;
-    ExtrinsicOpticToUser extrisic_optic_to_user;
-    IntrinsicCalibration intrinsic_calibration;
-    InverseIntrinsicCalibration inverse_intrinsic_calibration;
-    std::array<uint64_t, 3> exposure_timestamps_ns;
-    std::array<uint32_t, 3> exposure_times_s;
-    float illu_temperature;
-    std::array<char, 32> mode;
-    std::array<char, 32> imager;
-    const size_t tof_info_v3_size = 428;
+    uint32_t measurement_block_index;
+    float measurement_range_min;
+    float measurement_range_max;
+    const size_t tof_info_v4_size = 428;
 
-    static TofInfoV3
+    static TofInfoV4
     Deserialize(const Buffer& tof_info_buffer)
     {
-      TofInfoV3 tof_info_v3;
+      TofInfoV4 tof_info_v4;
 
-      tof_info_v3.Read(tof_info_buffer.ptr<uint8_t>(0),
+      tof_info_v4.Read(tof_info_buffer.ptr<uint8_t>(0),
                        tof_info_buffer.size());
-      return tof_info_v3;
+      return tof_info_v4;
     }
   };
 } // end namespace ifm3d
--- modules/deserialize/test/ifm3d-deserialize-tests.cpp
+++ modules/deserialize/test/ifm3d-deserialize-tests.cpp
@@ -9,13 +9,14 @@
 #include <ifm3d/device/err.h>
 #include <ifm3d/fg.h>
 #include <ifm3d/deserialize/struct_tof_info_v3.hpp>
+#include <ifm3d/deserialize/struct_tof_info_v4.hpp>
 #include <algorithm>
 #include <fstream>
 #include "tof_info_test_data.hpp"
 #include "test_utils.hpp"
 #include <limits>
 
-TEST(DeserializeTestWithFile, structv3_size_exception)
+TEST(DeserializeTestWithFile, struct_tof_info_v3_size_exception)
 {
   auto buffer = ifm3d::Buffer(1, 200, 1, ifm3d::pixel_format::FORMAT_8U);
 
@@ -27,17 +28,17 @@ TEST(DeserializeTestWithFile, struct_tof_info_v3)
   auto buffer = ifm3d::read_buffer_from_file("tof_info.data");
 
   auto tof_info_v3 = ifm3d::TofInfoV3::Deserialize(buffer);
-
-  EXPECT_GE(tof_info_v3.version, ifm3d::tof_info_v3::version);
+  constexpr auto minimum_required_version = 3;
+  EXPECT_GE(tof_info_v3.version, minimum_required_version);
   EXPECT_NEAR(tof_info_v3.amplitude_resolution,
-              ifm3d::tof_info_v3::amplitude_resolution,
+              ifm3d::tof_info::amplitude_resolution,
               ifm3d::epsilon);
   EXPECT_NEAR(tof_info_v3.distance_resolution,
-              ifm3d::tof_info_v3::distance_resolution,
+              ifm3d::tof_info::distance_resolution,
               ifm3d::epsilon);
   EXPECT_TRUE(
     ifm3d::compare_array(tof_info_v3.amp_normalization_factors,
-                         ifm3d::tof_info_v3::amp_normalization_factors));
+                         ifm3d::tof_info::amp_normalization_factors));
 
   std::array<float, 6> extrinc_opt_to_user = {
     tof_info_v3.extrisic_optic_to_user.transX,
@@ -49,36 +50,111 @@ TEST(DeserializeTestWithFile, struct_tof_info_v3)
 
   EXPECT_TRUE(
     ifm3d::compare_array(extrinc_opt_to_user,
-                         ifm3d::tof_info_v3::extrincsic_optic_to_user));
+                         ifm3d::tof_info::extrincsic_optic_to_user));
 
   EXPECT_EQ(tof_info_v3.intrinsic_calibration.modelID,
-            ifm3d::tof_info_v3::intrinsic_calib_model_id);
+            ifm3d::tof_info::intrinsic_calib_model_id);
 
   EXPECT_TRUE(
     ifm3d::compare_array(tof_info_v3.intrinsic_calibration.modelParameters,
-                         ifm3d::tof_info_v3::intrinsic_calib_model_param));
+                         ifm3d::tof_info::intrinsic_calib_model_param));
 
   EXPECT_EQ(tof_info_v3.inverse_intrinsic_calibration.modelID,
-            ifm3d::tof_info_v3::inverse_intrinsic_calib_model_id);
+            ifm3d::tof_info::inverse_intrinsic_calib_model_id);
 
   EXPECT_TRUE(ifm3d::compare_array(
     tof_info_v3.inverse_intrinsic_calibration.modelParameters,
-    ifm3d::tof_info_v3::inverse_intrinsic_calib_model_param));
+    ifm3d::tof_info::inverse_intrinsic_calib_model_param));
 
   EXPECT_TRUE(
     ifm3d::compare_array(tof_info_v3.exposure_timestamps_ns,
-                         ifm3d::tof_info_v3::exposure_timestamps_ns));
+                         ifm3d::tof_info::exposure_timestamps_ns));
 
   EXPECT_TRUE(ifm3d::compare_array(tof_info_v3.exposure_times_s,
-                                   ifm3d::tof_info_v3::exposure_times_s));
+                                   ifm3d::tof_info::exposure_times_s));
 
   EXPECT_NEAR(tof_info_v3.illu_temperature,
-              ifm3d::tof_info_v3::illu_temperature,
+              ifm3d::tof_info::illu_temperature,
               ifm3d::epsilon);
 
   EXPECT_TRUE(
-    ifm3d::compare_array(tof_info_v3.mode, ifm3d::tof_info_v3::mode));
+    ifm3d::compare_array(tof_info_v3.mode, ifm3d::tof_info::mode));
 
   EXPECT_TRUE(
-    ifm3d::compare_array(tof_info_v3.imager, ifm3d::tof_info_v3::imager));
+    ifm3d::compare_array(tof_info_v3.imager, ifm3d::tof_info::imager));
+}
+
+TEST(DeserializeTestWithFile, struct_tof_info_v4_size_exception)
+{
+  auto buffer = ifm3d::Buffer(1, 200, 1, ifm3d::pixel_format::FORMAT_8U);
+
+  EXPECT_THROW(ifm3d::TofInfoV4::Deserialize(buffer), ifm3d::Error);
+}
+TEST(DeserializeTestWithFile, struct_tof_info_v4)
+{
+
+  auto buffer = ifm3d::read_buffer_from_file("tof_info.data");
+
+  auto tof_info_v4 = ifm3d::TofInfoV4::Deserialize(buffer);
+  constexpr auto minimum_required_version = 4;
+  EXPECT_GE(tof_info_v4.version, minimum_required_version);
+  EXPECT_NEAR(tof_info_v4.amplitude_resolution,
+              ifm3d::tof_info::amplitude_resolution,
+              ifm3d::epsilon);
+  EXPECT_NEAR(tof_info_v4.distance_resolution,
+              ifm3d::tof_info::distance_resolution,
+              ifm3d::epsilon);
+  EXPECT_TRUE(
+    ifm3d::compare_array(tof_info_v4.amp_normalization_factors,
+                         ifm3d::tof_info::amp_normalization_factors));
+
+  std::array<float, 6> extrinc_opt_to_user = {
+    tof_info_v4.extrisic_optic_to_user.transX,
+    tof_info_v4.extrisic_optic_to_user.transY,
+    tof_info_v4.extrisic_optic_to_user.transZ,
+    tof_info_v4.extrisic_optic_to_user.rotX,
+    tof_info_v4.extrisic_optic_to_user.rotY,
+    tof_info_v4.extrisic_optic_to_user.rotZ};
+
+  EXPECT_TRUE(ifm3d::compare_array(extrinc_opt_to_user,
+                                   ifm3d::tof_info::extrincsic_optic_to_user));
+
+  EXPECT_EQ(tof_info_v4.intrinsic_calibration.modelID,
+            ifm3d::tof_info::intrinsic_calib_model_id);
+
+  EXPECT_TRUE(
+    ifm3d::compare_array(tof_info_v4.intrinsic_calibration.modelParameters,
+                         ifm3d::tof_info::intrinsic_calib_model_param));
+
+  EXPECT_EQ(tof_info_v4.inverse_intrinsic_calibration.modelID,
+            ifm3d::tof_info::inverse_intrinsic_calib_model_id);
+
+  EXPECT_TRUE(ifm3d::compare_array(
+    tof_info_v4.inverse_intrinsic_calibration.modelParameters,
+    ifm3d::tof_info::inverse_intrinsic_calib_model_param));
+
+  EXPECT_TRUE(ifm3d::compare_array(tof_info_v4.exposure_timestamps_ns,
+                                   ifm3d::tof_info::exposure_timestamps_ns));
+
+  EXPECT_TRUE(ifm3d::compare_array(tof_info_v4.exposure_times_s,
+                                   ifm3d::tof_info::exposure_times_s));
+
+  EXPECT_NEAR(tof_info_v4.illu_temperature,
+              ifm3d::tof_info::illu_temperature,
+              ifm3d::epsilon);
+
+  EXPECT_TRUE(ifm3d::compare_array(tof_info_v4.mode, ifm3d::tof_info::mode));
+
+  EXPECT_TRUE(
+    ifm3d::compare_array(tof_info_v4.imager, ifm3d::tof_info::imager));
+
+   EXPECT_EQ(tof_info_v4.measurement_block_index,
+            ifm3d::tof_info::measurement_block_index);
+  EXPECT_NEAR(tof_info_v4.measurement_range_min,
+               ifm3d::tof_info::measurement_range_min,
+               ifm3d::epsilon);
+   EXPECT_NEAR(tof_info_v4.measurement_range_max,
+               ifm3d::tof_info::measurement_range_max,
+               ifm3d::epsilon);
+
 }
