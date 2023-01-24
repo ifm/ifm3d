66e15b53a59f043792244dee4ec2dc5f86fa68ae
Sachin Bangadkar <Sachin.Bangadkar@ifm.com>
2023-01-24 12:49:51 +0530

    add test data and testcases for ods struct

---
+++ modules/deserialize/test/data/ods_info_test_data.hpp
@@ -0,0 +1,12 @@
+#include <array>
+
+namespace ifm3d
+{
+  namespace ods_info
+  {
+    constexpr uint64_t timestamp_ns = 1581093211432340979;
+    constexpr uint32_t zone_config_id = 0;
+    constexpr std::array<uint8_t, 3> zone_occupied = {0, 0, 0};
+
+  }
+}
---
+++ modules/deserialize/test/data/ods_occupancy_grid.hpp
@@ -0,0 +1,13 @@
+#include <array>
+
+namespace ifm3d
+{
+  namespace ods_occupancy_grid
+  {
+    constexpr uint64_t timestamp_ns = 1581093211432340979;
+    constexpr uint32_t width = 200;
+    constexpr uint32_t height = 200;
+    constexpr std::array<float, 6> transfor_cell_center_to_user =
+      {0.05000000075, 0, -4.974999905, 0, 0.05000000075, -4.974999905};
+  }
+}
