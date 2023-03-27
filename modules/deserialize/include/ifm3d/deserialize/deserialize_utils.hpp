462365783735a8533a0cb7868102c831cc82bd0c
Sachin Bangadkar <Sachin.Bangadkar@ifm.com>
2023-03-27 22:19:37 +0530

    add deserialize for O3D param

--- modules/deserialize/include/ifm3d/deserialize/deserialize_utils.hpp
+++ modules/deserialize/include/ifm3d/deserialize/deserialize_utils.hpp
@@ -20,6 +20,37 @@ namespace ifm3d
         element_index++;
       }
   }
+
+  template <typename T, size_t num_of_parameter>
+  class ArrayDeserialize
+  {
+  public:
+    void
+    Read(const uint8_t* data, size_t size)
+    {
+      if (size < (num_of_parameter * sizeof(T)))
+        {
+          throw ifm3d::Error(IFM3D_CORRUPTED_STRUCT);
+        }
+      const uint8_t* start_ptr = data;
+      mkarray<T, num_of_parameter>(start_ptr, param_vector);
+    };
+
+    /**
+     * @brief array to hold deserialize values
+     */
+    std::array<T, num_of_parameter> param_vector;
+
+    static auto
+    Deserialize(const Buffer& o3d_buffer)
+    {
+      ArrayDeserialize<T, num_of_parameter> param_values;
+
+      param_values.Read(o3d_buffer.ptr<uint8_t>(0), o3d_buffer.size());
+      return param_values;
+    }
+  };
+
 } // end namespace
 
 #endif // IFM3D_DESERIALIZE_UTILS_HPP
