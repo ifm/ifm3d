d330755d3f213cbaca9f7a30c32b40cd793ea6b2
Sachin Bangadkar <Sachin.Bangadkar@ifm.com>
2023-05-18 20:12:39 +0530

    add template contraint for format function in formatter

--- modules/common/include/ifm3d/common/logging/log_writer_console.h
+++ modules/common/include/ifm3d/common/logging/log_writer_console.h
@@ -30,7 +30,11 @@ namespace ifm3d
     StdErr,
   };
 
-  template <class Formatter>
+  template <class Formatter,
+            typename std::enable_if_t<std::is_same_v<
+              decltype(Formatter::format(
+                ifm3d::LogEntry("", ifm3d::LogLevel::Info, "", "", 1))),
+              std::string>>* = nullptr>
   class LogWriterConsole : public LogWriter
   {
   public:
