0a4491363dbd4c12d942a5cea2a05b65eec4ec30
Sachin Bangadkar <Sachin.Bangadkar@ifm.com>
2023-05-25 17:20:34 +0530

    make fmt as internal dependency

--- modules/common/test/logging/ifm3d-common-logging-tests.cpp
+++ modules/common/test/logging/ifm3d-common-logging-tests.cpp
@@ -6,10 +6,9 @@
 #include "gtest/gtest.h"
 #include <ifm3d/common/logging/logger.h>
 
-
 TEST(Logging, instance)
-{ 
- EXPECT_NO_FATAL_FAILURE( auto& logger = ifm3d::Logger::Get());
+{
+  EXPECT_NO_FATAL_FAILURE(auto& logger = ifm3d::Logger::Get());
 }
 
 TEST(Logging, get_default_value)
@@ -30,18 +29,12 @@ TEST(Logging, get_log_value)
   EXPECT_TRUE(logger.GetLogLevel() == ifm3d::LogLevel::Info);
 }
 
-
 TEST(Logging, shouldlog)
 {
   auto& logger = ifm3d::Logger::Get();
-  
+
   EXPECT_NO_FATAL_FAILURE(logger.SetLogLevel(ifm3d::LogLevel::Info));
   EXPECT_TRUE(logger.ShouldLog(ifm3d::LogLevel::Info));
   EXPECT_TRUE(logger.ShouldLog(ifm3d::LogLevel::Warning));
   EXPECT_FALSE(logger.ShouldLog(ifm3d::LogLevel::Verbose));
 }
-
-
-
-
-
