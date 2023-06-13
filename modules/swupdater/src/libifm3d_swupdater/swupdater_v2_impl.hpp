3e51ba440ba09ad3372f395857e023e3355c916c
Sachin Bangadkar <Sachin.Bangadkar@ifm.com>
2023-06-13 18:47:45 +0530

    update changelog

--- modules/swupdater/src/libifm3d_swupdater/swupdater_v2_impl.hpp
+++ modules/swupdater/src/libifm3d_swupdater/swupdater_v2_impl.hpp
@@ -58,8 +58,9 @@ namespace ifm3d
 
   /* curl parameters */
   const int SWUPDATE_V2_TIMEOUT_FOR_UPLOAD =
-    std::getenv("IFM3D_SWUPDATE_CURL_TIMEOUT") == nullptr ? 600 :
-                                         std::stoi(std::getenv("IFM3D_SWUPDATE_CURL_TIMEOUT"));
+    std::getenv("IFM3D_SWUPDATE_CURL_TIMEOUT") == nullptr ?
+      600 :
+      std::stoi(std::getenv("IFM3D_SWUPDATE_CURL_TIMEOUT"));
   const int CURL_MAX_REDIR = 50;
   //============================================================
   // Impl interface
