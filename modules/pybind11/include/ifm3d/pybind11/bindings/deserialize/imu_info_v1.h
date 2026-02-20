aa678eea8a6cd8ebaf1183443a775a59f84458fe
Patil <suraj.patil@ifm.com>
2026-02-20 13:08:03 +0530

    :bug: Fix IMUInfoV1.imu_samples return type conversion Correct pybind11 binding so imu_samples returns a valid Python list instead of raising a conversion error.
    Signed-off-by: Patil <suraj.patil@ifm.com>

--- modules/pybind11/include/ifm3d/pybind11/bindings/deserialize/imu_info_v1.h
+++ modules/pybind11/include/ifm3d/pybind11/bindings/deserialize/imu_info_v1.h
@@ -6,14 +6,70 @@
 #ifndef IFM3D_PYBIND_BINDING_DESERIALIZE_IMU_INFO_V1_H
 #define IFM3D_PYBIND_BINDING_DESERIALIZE_IMU_INFO_V1_H
 
+#include <ifm3d/deserialize/struct_calibration.hpp>
 #include <ifm3d/deserialize/struct_imu_info_v1.hpp>
 #include <pybind11/numpy.h>
 #include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
 namespace py = pybind11;
 
 inline void
 bind_struct_imuinfov1(pybind11::module_& m)
 {
+  py::class_<ifm3d::calibration::IMUSample, ifm3d::calibration::IMUSample::Ptr>
+    imu_samples(m,
+                "IMUSample",
+                "R(Class for managing an instance of imu sample data)");
+
+  imu_samples.def(py::init<>(),
+                  R"(
+        Constructor
+      )");
+  imu_samples.def_readonly("hw_timestamp",
+                           &ifm3d::calibration::IMUSample::hw_timestamp,
+                           R"(
+        Hardware timestamp of the IMU sample
+      )");
+  imu_samples.def_readonly("timestamp",
+                           &ifm3d::calibration::IMUSample::timestamp,
+                           R"(
+        Timestamp of the IMU sample
+      )");
+  imu_samples.def_readonly("temperature",
+                           &ifm3d::calibration::IMUSample::temperature,
+                           R"(
+        Temperature at the time of the IMU sample
+      )");
+  imu_samples.def_readonly("acc_x",
+                           &ifm3d::calibration::IMUSample::acc_x,
+                           R"(
+        Accelerometer X-axis data in [m/s^2]
+      )");
+  imu_samples.def_readonly("acc_y",
+                           &ifm3d::calibration::IMUSample::acc_y,
+                           R"(
+        Accelerometer Y-axis data in [m/s^2]
+      )");
+  imu_samples.def_readonly("acc_z",
+                           &ifm3d::calibration::IMUSample::acc_z,
+                           R"(
+        Accelerometer Z-axis data in [m/s^2]
+      )");
+  imu_samples.def_readonly("gyro_x",
+                           &ifm3d::calibration::IMUSample::gyro_x,
+                           R"(
+        Gyroscope X-axis data in [rad/s]
+      )");
+  imu_samples.def_readonly("gyro_y",
+                           &ifm3d::calibration::IMUSample::gyro_y,
+                           R"(
+        Gyroscope Y-axis data in [rad/s]
+      )");
+  imu_samples.def_readonly("gyro_z",
+                           &ifm3d::calibration::IMUSample::gyro_z,
+                           R"(
+        Gyroscope Z-axis data in [rad/s]
+      )");
 
   py::class_<ifm3d::IMUInfoV1, ifm3d::IMUInfoV1::Ptr> imu_info_v1(m,
                                                                   "IMUInfoV1",
